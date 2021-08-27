5831fbb392251d47f0bd067d177fdacdabc69ff5
luismaguilar <84119255+luismaguilar@users.noreply.github.com>
2021-08-26 19:16:14 -0500

    update

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -18,6 +18,7 @@ export class NavbarComponent implements OnInit {
     const navbar: HTMLElement = this.element.nativeElement
     this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0]
   }
+  
   sidebarOpen() {
     const toggleButton = this.toggleButton
     const html = document.getElementsByTagName('html')[0]
